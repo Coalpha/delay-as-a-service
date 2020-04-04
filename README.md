be9f8af95443e0eba755ec645a736c583d2b357a
Coalpha <arcanehood@gmail.com>
2020-04-04 00:34:25 -0700

    (ES)Linting
    Changed a little bit of the code because the example was a little
    unclear. Used ES6 modules as well.

--- README.md
+++ README.md
@@ -2,14 +2,11 @@
 
 *You heard it here first, folks*
 
-Pausing the JavaScript event loop is almost always a bad idea.
-As you can see, this hasn't stopped me.
-The way this works is by sending a synchronous `XMLHttpRequest` with a query
-of how long the server should wait to send something back.
-This method does, of course, require a server of some sorts.
+The way this works is by sending a synchronous `XMLHttpRequest` to a server that
+you run. The client specifies how long the server should wait to respond in the
+query string.
+This does, of course, require a server of some sorts.
 Theoretically, the server could be implemented using a service worker so
 that latency is no longer an issue. I'll leave that up to someone else to do.
 
-### Check out the example webpage:
-
-[![Click me, I'm a link!](example/example.png "The example DAAS webpage")](example)
+[Run the example: ![](example/example.png "The example DAAS webpage")](example)
--- example/example.js
+++ example/example.js
@@ -1,32 +1,45 @@
+import sleep_ms from "../lib/client.js";
+
 const buttongroup = document.getElementById("buttongroup");
 const timediv = document.getElementById("time");
 
-for (let button of buttongroup.children) {
-   const buttonSeconds = button.id[5];
-   const unit = buttonSeconds == 1 ? "second" : "seconds";
+/**
+ * @param {Number} seconds
+ */
+function sleep_s(seconds) {
+   sleep_ms(seconds * 1000);
+}
+
+for (const button of buttongroup.children) {
+   const buttonSeconds = +button.getAttribute("data-sleep");
+
+   const unit = buttonSeconds === 1 ? "second" : "seconds";
+
    button.innerText = `Sleep for ${buttonSeconds} ${unit}`;
-   const buttonMS = buttonSeconds * 1000;
    button.addEventListener("click", () => {
-      timediv.className = "paused";
-      sleep(buttonMS);
-      timediv.className = "";
+      timediv.setAttribute("class", "clock-paused");
+      sleep_s(buttonSeconds);
+      timediv.removeAttribute("class");
    });
 }
 
-
+/**
+ * @param {Number} int
+ * @returns {String}
+ */
 function asTime(int) {
    return int.toString().padStart(2, "0");
 }
 
-function updateTimediv() {
+function updateTime() {
    const now = new Date;
-   const hours = asTime(now.getHours())
+   const hours = asTime(now.getHours());
    const minutes = asTime(now.getMinutes());
    const seconds = asTime(now.getSeconds());
 
    timediv.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
-updateTimediv();
+updateTime();
 
-setInterval(updateTimediv, 1000);
+setInterval(updateTime, 1000);
